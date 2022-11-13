db6c7916fb14b15006974319feba3434c0dbc1ea
ashgolan <ashgolan@hotmail.com>
2022-11-13 14:34:45 +0200

    adding keys to cubes

--- src/components/Cubes.js
+++ src/components/Cubes.js
@@ -1,9 +1,8 @@
 import "./Cubes.css";
 
 function Cubes(props) {
-  console.log(props);
-  const cubes = props.randomCubes.map((cubes) => {
-    return <img className="cube__img" src={cubes}></img>;
+  const cubes = props.randomCubes.map((cubes, index) => {
+    return <img key={`c${index}`} className="cube__img" src={cubes}></img>;
   });
 
   return <div className="cubes__container">{cubes}</div>;
